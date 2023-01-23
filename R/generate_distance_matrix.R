04237f2928a86054960a32ee5967d71121faf313
José Conejeros <jdconejeros@uc.cl>
2023-01-23 11:28:19 -0300

    Script files functions packages

---
+++ R/generate_distance_matrix.R
@@ -0,0 +1,11 @@
+#' Function Test
+    #'
+    #' General descriptions
+    #' @param x arguments
+    #'
+    #' @return results
+    #' @export
+    #'
+    #' @examples
+    #' x <- c(1,2,3)
+    #' function
